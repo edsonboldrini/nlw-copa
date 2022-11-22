485794d3ca951cb6d03aaae804cee138a6ec063a
Edson Boldrini <edsonboldrini@gmail.com>
2022-11-22 00:46:54 -0300

    Update seeds

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,48 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'John Doe',
-      email: 'john.doe@gmail.com',
-      avatarUrl: 'github.com/edsonboldrini.png',
-    }
-  })
-
-  const pool = await prisma.pool.create({
-    data: {
-      title: 'Bolão 123',
-      code: 'BOL123',
-      ownerId: user.id,
-
-      participants: {
-        create: {
-          userId: user.id
-        }
-      }
-    }
-  })
-
   const game1 = await prisma.game.create({
     data: {
-      date: '2022-11-24T16:00:00.000Z',
+      date: '2022-11-24T19:00:00.000Z',
       firstTeamCountryCode: 'BR',
-      secondTeamCountryCode: 'CH',
-
-      guesses: {
-        create: {
-          firstTeamPoints: 2,
-          secondTeamPoints: 0,
-          participant: {
-            connect: {
-              userId_poolId: {
-                userId: user.id,
-                poolId: pool.id,
-              }
-            }
-          }
-        }
-      }
+      secondTeamCountryCode: 'RS',
     }
   })
 
@@ -52,7 +15,15 @@ async function main() {
     data: {
       date: '2022-11-28T16:00:00.000Z',
       firstTeamCountryCode: 'BR',
-      secondTeamCountryCode: 'RS',
+      secondTeamCountryCode: 'CH',
+    }
+  })
+
+  const game3 = await prisma.game.create({
+    data: {
+      date: '2022-12-02T19:00:00.000Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'CM',
     }
   })
 }
